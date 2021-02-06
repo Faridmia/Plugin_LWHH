bae6bbcbbf3aa6e2eff2d9b14b8ca3a798ce03cf
faridmia <faridcse7800@gmail.com>
2021-02-06 23:40:12 +0600

    db list table pagination

--- wp-content/plugins/database-demo/class.dbdemousers.php
+++ wp-content/plugins/database-demo/class.dbdemousers.php
@@ -1,2 +1,64 @@
 <?php
+    if ( ! defined( 'ABSPATH' ) ) { exit; // Exit if accessed directly 
+
+    } 
+    if ( ! class_exists( 'WP_List_Table' ) ) { 
+        require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php'; 
+    }
+
+    class DBTableUsers extends WP_List_Table {
+        private $_items;
+        function __construct( $data ) {
+            parent::__construct();
+            $this->_items = $data;
+        }
+    
+        function get_columns() {
+            return [
+                'cb'     => '<input type="checkbox">',
+                'name'   => __( 'Name', 'database-demo' ),
+                'email'  => __( 'Email', 'database-demo' ),
+                'action' => __( 'Action', 'database-demo' ),
+            ];
+        }
+    
+        function column_cb( $item ) {
+            return "<input type='checkbox' value='{$item['id']}'>";
+        }
+
+        function column_name($item){
+            $nonce = wp_create_nonce("dbdemo_edit");
+            $actions = [
+                'edit' => sprintf('<a href="?page=dbdemo&p_id=%s&n=%s">%s<a>',$item['id'],$nonce,__( 'Edit', 'database-demo' )),
+                'delete' => sprintf('<a href="?page=dbdemo&p_id=%s&n=%s&action=%s">%s<a>',$item['id'],$nonce,'delete',__( 'Delete', 'database-demo' )),
+
+            ];
+            return sprintf("%s %s",$item['name'],$this->row_actions($actions));
+        }
+
+        function column_action($item){
+            $link = wp_nonce_url(admin_url("?page=dbdemo&p_id=").$item['id'],"dbdemo_edit","n"); // dedemo hoitece action ar name r n hoitece url a ki name jabe
+            return "<a href='".esc_url($link)."'>Edit</a>";
+        }
+    
+        function column_default( $item, $column_name ) {
+            return $item[ $column_name ];
+        }
+    
+        function prepare_items() {
+
+            $per_page = 2;
+            $current_page = $this->get_pagenum();
+            $total_items = count($this->_items);
+            $this->set_pagination_args([
+                'total_items' => $total_items,
+                'per_page' => $per_page
+            ]);
+
+            $data = array_slice($this->_items,($current_page-1)*$per_page,$per_page);
+            $this->items =$data;
+            $this->_column_headers = array( $this->get_columns(), [], [] );
+        }
+    }
+    
 
--- wp-content/plugins/database-demo/dbdemo.php
+++ wp-content/plugins/database-demo/dbdemo.php
@@ -12,6 +12,8 @@ Domain Path: /languages/
 */
 define("DBDEMO_DB_VERSION","1.2");
 
+require_once 'class.dbdemousers.php';
+
 function dbdemo_init(){
 	global $wpdb;
 	$prefix = 'wp_';
@@ -78,6 +80,12 @@ function dbdemo_load_data(){
 	]);
 }
 
+add_action("admin_enqueue_scripts",function($hook){
+	if("toplevel_page_dbdemo" == $hook){
+		wp_enqueue_style("dbdemo-css",plugin_dir_url(__FILE__) . "assets/css/form.css");
+	}
+});
+
 register_activation_hook(__FILE__,"dbdemo_load_data");
 
 register_deactivation_hook(__FILE__,"dbdemo_flush_data");
@@ -97,8 +105,22 @@ add_action("admin_menu",function(){
 });
 
 function dbdemo_admin_page(){
-
 	global $wpdb;
+	$table_name = $wpdb->prefix . 'person';
+	if(isset($_GET['p_id'])){
+		if(!isset($_GET['n']) || !wp_verify_nonce($_GET['n'],"dbdemo_edit")){ // dbdemo_edit hoitece action ar nam
+			wp_die(__("You are not authorized to do this","database-demo"));
+		}
+
+		if(isset($_GET['action']) && $_GET['action'] == 'delete'){
+			$id = sanitize_key($_GET['p_id']);
+			$wpdb->delete("{$table_name}",['id' => $id]);
+
+			$_GET['p_id'] = null;
+		}
+	}
+
+	
 	$table_name = $wpdb->prefix . 'person';
 
 	$id = $_GET['p_id'] ?? 0;
@@ -156,6 +178,15 @@ function dbdemo_admin_page(){
 			<?php _e( 'Users', 'database-demo' ) ?>
         </div>
         <div class="form_box_content">
+				<?php 
+				
+					global $wpdb;
+					$dbdemo_users = $wpdb->get_results("SELECT id,name,email FROM {$wpdb->prefix}person",ARRAY_A);
+					//print_r($dbdemo_users);
+					$dbtable = new DBTableUsers($dbdemo_users);
+					$dbtable->prepare_items();
+					$dbtable->display();
+				?>
         </div>
     </div>
 	<?php
@@ -177,11 +208,14 @@ add_action("admin_post_dbdemo_add_record",function(){
 
 			if($id){
 				$wpdb->update("{$table_name}",['name' => $name,'email' => $email],['id' => $id]); 
-				wp_redirect(admin_url("admin.php?page=dbdemo&p_id=".$id));
+
+				$nonce = wp_create_nonce('dbdemo_edit');
+				wp_redirect(admin_url("admin.php?page=dbdemo&p_id=").$id . "&n={$nonce}");
 			}else{
 				$wpdb->insert("{$table_name}",['name' => $name,'email' => $email]); 
-				//$new_id = $wpdb->insert_id;
-				wp_redirect(admin_url("admin.php?page=dbdemo&p_id=".$new_id));
+				// $new_id = $wpdb->insert_id;
+				// wp_redirect(admin_url("admin.php?page=dbdemo&p_id=".$new_id));
+				wp_redirect(admin_url("admin.php?page=dbdemo"));
 			}
 			
 		}
